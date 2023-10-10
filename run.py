646c61d0e04d85d89a71713393ad0ee719c96282
Jimmy Hendrix <jimmythedestroyer@gmail.com>
2023-10-09 20:55:22 -0700

    setup gymnasium environment

---
+++ run.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+import gymnasium as gym
+env = gym.make("SpaceInvaders-v0", render_mode="human")
+observation, info = env.reset()
+
+for _ in range(1000):
+    action = env.action_space.sample()  # agent policy that uses the observation and info
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
