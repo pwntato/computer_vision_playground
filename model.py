9ef4934a0ef0357be792adfa51cfe48e0add9fca
Jimmy Hendrix <jimmythedestroyer@gmail.com>
2023-10-11 21:02:48 -0700

    add optional hidden layers

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ class SpaceInvadersModel(nn.Module):
     super(SpaceInvadersModel, self).__init__()
 
     nfs = (1, 4, 8, 16, 32, 64, 128, 256, 512) # 210x160 -> 105x80 -> 53x40 -> 27x20 -> 14x10 -> 7x5 -> 4x3 -> 2x2 -> 1x1
+    hidden_layers = 2
     norm = nn.BatchNorm2d
     activate = nn.SiLU
     stride = 2
@@ -18,10 +19,14 @@ class SpaceInvadersModel(nn.Module):
         self.layers.append(nn.Conv2d(nfs[i], nfs[i + 1], kernel_size=kernel_size, stride=stride, padding=padding, bias=True))
         if i < len(nfs) - 2: # last layer isn't 2D
             self.layers.append(norm(nfs[i + 1]))
-        self.layers.append(activate())
+            self.layers.append(activate())
     self.layers.append(nn.Flatten())
-    # add history of N moves and possible an extra hidden layer or two
+    # add history of N moves
+    for i in range(hidden_layers):
+        self.layers.append(nn.Linear(nfs[-1], nfs[-1]))
+        self.layers.append(activate())
     self.layers.append(nn.Linear(nfs[-1], n_actions))
+    self.layers.append(nn.ReLU())
     self.layers.append(nn.Softmax(dim=1))
 
   def forward(self, x):
