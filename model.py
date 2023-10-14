778808101296efd2e212c4a91ea43d8180d05248
Jimmy Hendrix <jimmythedestroyer@gmail.com>
2023-10-13 23:44:47 -0700

    conv to resnet

--- model.py
+++ model.py
@@ -2,13 +2,28 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+class ResidualBlock(nn.Module):
+    def __init__(self, ni, nf, kernel_size=3, stride=1, padding=1, bias=True, norm=nn.BatchNorm2d, activate=nn.SiLU):
+        super(ResidualBlock, self).__init__()
+        self.layers = nn.Sequential()
+        self.layers.append(nn.Conv2d(ni, nf, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias))
+        if norm is not None:
+            self.layers.append(norm(nf))
+        if activate is not None:
+            self.layers.append(activate())
+        self.identity = nn.Conv2d(ni, nf, kernel_size=1, stride=stride, padding=0, bias=bias)
+
+    def forward(self, x):
+        return self.layers(x) + self.identity(x)
+
+
 class SpaceInvadersModel(nn.Module):
   def __init__(self, n_actions=6, frames=3):
     super(SpaceInvadersModel, self).__init__()
 
     nfs = (frames, 8, 16, 32, 64, 128, 256, 512, 1024) # 210x160 -> 105x80 -> 53x40 -> 27x20 -> 14x10 -> 7x5 -> 4x3 -> 2x2 -> 1x1
     hidden_layers = 2
-    norm = nn.BatchNorm2d
+    normalize = nn.BatchNorm2d
     activate = nn.SiLU
     stride = 2
     kernel_size = 3
@@ -16,11 +31,9 @@ class SpaceInvadersModel(nn.Module):
 
     self.layers = nn.Sequential()
     for i in range(len(nfs) - 1):
-        # possibly replace with residual block
-        self.layers.append(nn.Conv2d(nfs[i], nfs[i + 1], kernel_size=kernel_size, stride=stride, padding=padding, bias=True))
-        if i < len(nfs) - 2: # last layer isn't 2D
-            self.layers.append(norm(nfs[i + 1]))
-            self.layers.append(activate())
+        norm = normalize if i < len(nfs) - 2 else None
+        act = activate if i < len(nfs) - 2 else None
+        self.layers.append(ResidualBlock(nfs[i], nfs[i + 1], kernel_size=kernel_size, stride=stride, padding=padding, bias=True, norm=norm, activate=act))
     self.layers.append(nn.Flatten())
     # add history of N moves
     for i in range(hidden_layers):
@@ -34,6 +47,6 @@ class SpaceInvadersModel(nn.Module):
         #print(layer, x.shape)
         x = layer(x)
         #print(x)
-    print(x)
+    #print(x)
     return x
   
