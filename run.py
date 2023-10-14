204c3ed5700000d3e64d1b6a8580feadd4686d33
Jimmy Hendrix <jimmythedestroyer@gmail.com>
2023-10-13 23:11:15 -0700

    notes on future improvements

--- run.py
+++ run.py
@@ -12,6 +12,10 @@ from collections import deque
 from model import SpaceInvadersModel
 from util import prep_observation_for_model, q_values_to_action, frames_to_tensor
 
+# Residual blocks
+# Random batches of frame stacks
+# Pass action history to model
+
 human = False
 view_scale = 4
 
