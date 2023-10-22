903dd0932f35e658c1fd61fa22cb13f9712a96d8
Jimmy Hendrix <jimmythedestroyer@gmail.com>
2023-10-21 22:04:57 -0700

    first pass at monte carlo

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from collections import deque
 
 from model import AtariModel
-from util import prep_observation_for_model, q_values_to_action, frames_to_tensor, random_stack_sample, get_sample_stack
+from util import prep_observation_for_model, frames_to_tensor, get_sample_stack
 from game_util import render_frame
 
 view_scale = 4
@@ -40,14 +40,10 @@ screen = pygame.display.set_mode(((width * view_scale) + 400, height * view_scal
 font = pygame.font.Font(pygame.font.get_default_font(), 36)
 
 frames = deque(maxlen=frame_count)
-frame_stack_history = {
-    "state": deque(maxlen=keep_frame_stack_history),
-    "next_state": deque(maxlen=keep_frame_stack_history),
-    "reward": deque(maxlen=keep_frame_stack_history),
-}
 
 observation, info = env.reset()
 
+episode = []
 action = 0 # no action
 state = prep_observation_for_model(observation, device)
 for _ in range(frame_count):
@@ -70,56 +66,47 @@ while running:
         else:
             with torch.no_grad():
                 q_values = model(frames_to_tensor(frames).unsqueeze(0).to(device))
-                action = q_values_to_action(q_values)
+                action = q_values.argmax().item()
+                #action = torch.distributions.Categorical(q_values).sample().item()
                 #print(f"q_values: {q_values} action: {action}")
 
     # take action in environment
-    observation, reward, terminated, truncated, info = env.step(action)
+    observation, reward, terminated, truncated, _ = env.step(action)
     next_state = prep_observation_for_model(observation, device)
     score += reward
     frame_skip_reward += reward
     reward = frame_skip_reward
-
-    # update model
-    if frame_number % skip_frames == 0 or terminated or truncated:
-        # need to have random sample and compare to random sample next
-        random_state_sample, random_next_state_sample, random_reward_sample = random_stack_sample(frame_stack_history, batch_size-1, device)
-
-        state_stack = frames_to_tensor(frames).clone().unsqueeze(0).to(device)
-        frame_stack_history["state"].append(state_stack)
-        frames.append(next_state)
-        next_state_stack = frames_to_tensor(frames).clone().unsqueeze(0).to(device)
-        frame_stack_history["next_state"].append(next_state_stack)
-        reward = torch.tensor(reward, device=device, dtype=torch.float32, requires_grad=True).unsqueeze(0).to(device)
-        frame_stack_history["reward"].append(reward)
-
-        state_stack_sample = get_sample_stack(random_state_sample, state_stack)
-        q_values = model(state_stack_sample)
-        q_value = q_values[:, action]
-        q_value.requires_grad_(True)
-        #print(f"q_value: {q_value.shape}")
-
-        # run through model
-        next_state_stack_sample = get_sample_stack(random_next_state_sample, next_state_stack)
-        next_q_values = model(next_state_stack_sample)
-        reward_stack_sample = get_sample_stack(random_reward_sample, reward.unsqueeze(0)).flatten()
-        next_q_values = next_q_values.max(dim=1)[0]
-        target_q_value = reward_stack_sample + discount * next_q_values * (not terminated)
-        if len(target_q_value.shape) > 1:
-            target_q_value = target_q_value.squeeze(-1)
-        target_q_value.requires_grad_(True)
-
-        loss = loss_function(q_value, target_q_value)
-        #print(f"q_value: {q_value} target_q_value: {target_q_value} loss: {loss}")
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    episode.append((frames_to_tensor(frames), action, reward))
 
     state = next_state
     frame_number += 1
 
     if terminated or truncated:
+        # Adjust model weights, monte carlo style
+        returns = []
+        g_return = 0
+        for i in range(len(episode) - 1, -1, -1):
+            stack, action, reward = episode[i]
+            g_return = reward + discount * g_return
+            returns.append((stack, action, g_return))
+        returns.reverse()
+
+        for i in range(0, len(returns), batch_size):
+            batch = returns[i:i+batch_size]
+            stack_batch = torch.stack([x[0] for x in batch]).to(device)
+            action_batch = torch.tensor([x[1] for x in batch], device=device)
+            g_return_batch = torch.tensor([x[2] for x in batch], device=device)
+
+            print(f"stack_batch: {stack_batch.shape} action_batch: {action_batch.shape} g_return_batch: {g_return_batch.shape}")
+
+            q_values = model(stack_batch)
+            q_values = q_values.gather(1, action_batch.unsqueeze(1)).squeeze(1)
+
+            loss = loss_function(q_values, g_return_batch)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
         # Game over, reset tracking variables
         if score > high_score:
             high_score = score
--- util.py
+++ util.py
@@ -7,28 +7,9 @@ def prep_observation_for_model(observation, device):
     #result = result - result.mean()
     return result
 
-def q_values_to_action(q_values):
-    return q_values.argmax().item()
-    #return torch.distributions.Categorical(q_values).sample().item()
-
 def frames_to_tensor(frames):
     result = torch.stack(tuple(frames))
     return result
-
-def random_stack_sample(frame_stack_history, batch_size, device):
-    state = []
-    next_state = []
-    reward = []
-    for _ in range(min(batch_size, len(frame_stack_history["state"]))):
-        index = random.randint(0, len(frame_stack_history["state"])-1)
-        state.append(frame_stack_history["state"][index])
-        next_state.append(frame_stack_history["next_state"][index])
-        reward.append(frame_stack_history["reward"][index].unsqueeze(0))
-    if len(state) > 0:
-        state = torch.cat(state, dim=0).to(device)
-        next_state = torch.cat(next_state, dim=0).to(device)
-        reward = torch.cat(reward, dim=0).to(device)
-    return (state, next_state, reward)
 
 def get_sample_stack(random_sample, current_value):
     stack_sample = None
